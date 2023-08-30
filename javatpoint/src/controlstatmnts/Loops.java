407509cad234d09f759dda5a6bd65e42cf27c19b
Mohd Ziyauddin <mohd.ziyauddin741@gmail.com>
2023-08-30 21:20:21 +0530

    added code for break based upon name

--- javatpoint/src/controlstatmnts/Loops.java
+++ javatpoint/src/controlstatmnts/Loops.java
@@ -6,16 +6,47 @@ public class Loops {
 		for (int i = 0; i <= 10; i++) {
 			sum += i;
 		}
-		
+
 		System.out.println("Sum of first 10 numbers ==== " + sum);
-		
-		String[] languageNames = {"C", "C++", "Java", "Python", "Javascript"};
-		
+
+		String[] languageNames = { "C", "C++", "Java", "Python", "Javascript" };
+
 		System.out.println("Printing the content of array elements");
-		
+
 		for (String languageName : languageNames) {
 			System.out.println(languageName);
 		}
 
+		// *************************************************************************
+
+		int i = 0;
+		System.out.println("Printing the first even numbers upto 10 : ");
+		while (i <= 10) {
+			System.out.println(i);
+			i += 2;
+		}
+
+		// ************************************************************************
+
+		i = 0;
+		System.out.println("Printing the first even numbers upto 10 using do while loop : ");
+		do {
+			System.out.println(i);
+			i = i + 2;
+		} while (i <= 10);
+
+		// ************************************************************************
+
+		a: for (int k = 0; k <= 10; k++) {
+			b: for (int m = 0; m <= 15; m++) {
+				c: for (int n = 0; n <= 20; n++) {
+					System.out.println(n);
+					if (n == 7) {
+						break a;
+					}
+				}
+			}
+
+		}
 	}
 }
