a05393e6d3d3e6347ea086c2983c8c5a68e29ac8
Mohd Ziyauddin <mohd.ziyauddin741@gmail.com>
2023-08-30 15:14:53 +0530

    added if else code

--- javatpoint/src/controlstatmnts/Student.java
+++ javatpoint/src/controlstatmnts/Student.java
@@ -4,21 +4,39 @@ public class Student {
 
 	public static void main(String[] args) {
 		String city = "Delhi";
-		
+
 		if (city == "Meerut") {
 			System.out.println("city is meerut");
 		}
-		
+
 		else if(city == "Noida") {
 			System.out.println("city is noida");
 		}
-		
+
 		else if (city == "Agra") {
 			System.out.println("city is agra");
 		}
-		
+
 		else
 			System.out.println(city);
-	}
 
-}
+		// ***********************************************************************
+
+		String addressingaa = "Delhi, India";
+
+		if(addressingaa.endsWith("India")) {
+			if (addressingaa.contains("Meerut")) {
+				System.out.println("Your city is Meerut");
+
+			} else if (addressingaa.contains("Noida") ){
+				System.out.println("Your city is Noida");
+			}
+				else System.out.println(addressingaa.split(",")[0]);
+			}
+
+			else {
+				System.out.println("Your not living in India");
+			}
+		}
+
+	}
