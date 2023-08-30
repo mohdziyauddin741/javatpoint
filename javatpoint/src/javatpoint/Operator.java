2ab341d755f728188abc3c6b3be6e0d0be2d3871
Mohd Ziyauddin <mohd.ziyauddin741@gmail.com>
2023-08-30 13:23:34 +0530

    first commit of the project

---
+++ javatpoint/src/javatpoint/Operator.java
@@ -0,0 +1,13 @@
+package javatpoint;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 20;
+		int min = (a < b)? a : b;
+		System.out.println(min);
+
+	}
+
+}
