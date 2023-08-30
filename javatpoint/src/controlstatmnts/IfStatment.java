452de7416767262789df4ab2fa55e8d56f7e987c
Mohd Ziyauddin <mohd.ziyauddin741@gmail.com>
2023-08-30 14:09:27 +0530

    added student class

---
+++ javatpoint/src/controlstatmnts/IfStatment.java
@@ -0,0 +1,11 @@
+package controlstatmnts;
+
+public class IfStatment {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		if (x + y > 20) {
+			System.out.println("x + y is greater than 20");
+		}
+	}
+}
